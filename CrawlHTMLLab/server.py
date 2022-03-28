86532e264ec0881950c92574cb1034df9db74dde
lialic <19520208@gm.uit.edu.vn>
2022-03-29 01:04:16 +0700

    Added enums and Tin's code for crawling facebook post :sparkles:

--- CrawlHTMLLab/server.py
+++ CrawlHTMLLab/server.py
@@ -1,29 +1,39 @@
 from flask import Flask, render_template, request, redirect, url_for
 from crawler import facebook, images, news, researchPaper
+from static.enums.constant import CRAWLING_OPTIONS, SELECT_VALUE
+
 
 app = Flask(__name__)
 
-select_value = {
-  1: "ResearchPaper",
-  2: "News",
-  3: "Facebook",
-  4: "Google Image"
-}
-
 @app.route('/')
 def index():
-  return render_template('index.html')
+  return render_template('index.html', crawlOptions=CRAWLING_OPTIONS)
 
 @app.route('/result', methods=['POST'])
 def result():
-  chose_option = int(request.form['crawlOptions'])
-  result_value = {
+  option = int(request.form['crawlOptions'])
+  
+  if option == CRAWLING_OPTIONS.FACEBOOK.value:
+    crawl_result = facebook.crawl(request.form['keyword'], request.form['password'], request.form['target-page-input'], request.form['post-num'])
+    
+    js = open('./static/results.js', 'r')
+    js_out = f"var crawl_result = JSON.parse('{crawl_result}');"
+    
+    result_value = {
+      "target": request.form['target-page-input'],
+      "crawlOption": SELECT_VALUE[option],
+      "username": request.form['keyword'],
+      "result": crawl_result,
+    }
+  else:
+    result_value = {
     "keyword": request.form['keyword'],
-    "crawlOption": select_value[chose_option],
+    "crawlOption": SELECT_VALUE[option],
     "result": {
       "json": "json",
     }
   }
+  
   return render_template('result.html', result=result_value)
 
 
--- CrawlHTMLLab/static/index.js
+++ CrawlHTMLLab/static/index.js
@@ -1,22 +1,54 @@
+import * as constant from './enums/constant.js';
+
 document.addEventListener("DOMContentLoaded", function(event) { 
     let selectCrawlOption = document.getElementById("crawlOptions");
     let inputKeyword = document.getElementById("keyword");
+    let labelKeyword = document.getElementById("label-keyword");
+    let inputPassword = document.getElementById("password");
+    let labelPassword = document.getElementById("label-password");
+    let targetPage = document.getElementById("target-page");
+    let inputTargetPage = document.getElementById("target-page-input");
+    let inputQuantity = document.getElementById("post-num");
+
+    let condition = (inputKeyword.value.length > 0 && inputPassword.value.length > 0 && inputTargetPage.value.length > 0 && inputQuantity.value.length > 0);
+
+    const checkValidForm = () => {
+        if(condition || (inputPassword.hidden && inputKeyword.value.length > 0))
+                document.getElementById("submitBtn").disabled = false;
+        else 
+            document.getElementById("submitBtn").disabled = true;
+    }
 
     selectCrawlOption.addEventListener("change", () => {
-        inputKeyword.disabled = false
+        inputKeyword.disabled = false;
+        inputPassword.hidden = labelPassword.hidden = targetPage.hidden = true;
+        let requireField = '<span style="color:red">* </span>';
 
-        if (selectCrawlOption.value === "1")
-            inputKeyword.placeholder = 'Enter author name';
-        else 
-            inputKeyword.placeholder = 'Enter keyword';
-    })
-
-    inputKeyword.addEventListener("keyup", () => {
-        if (inputKeyword.value.length > 0) {
-            document.getElementById("submitBtn").disabled = false;
-        } else {
-            document.getElementById("submitBtn").disabled = true;
+        switch (Number(selectCrawlOption.value)) {
+            case constant.CrawlingOptions.RESEARCH_PAPER.value:
+                inputKeyword.placeholder = constant.CrawlingOptions.RESEARCH_PAPER.placeholder;
+                labelKeyword.innerHTML = requireField + constant.CrawlingOptions.RESEARCH_PAPER.inputLabel;
+                break;
+            case constant.CrawlingOptions.NEWS.value:
+                inputKeyword.placeholder = constant.CrawlingOptions.NEWS.placeholder;
+                labelKeyword.innerHTML = requireField + constant.CrawlingOptions.NEWS.inputLabel;
+                break;
+            case constant.CrawlingOptions.FACEBOOK.value:
+                inputKeyword.placeholder = constant.CrawlingOptions.FACEBOOK.placeholder;
+                labelKeyword.innerHTML = requireField + constant.CrawlingOptions.FACEBOOK.inputLabel;
+                inputPassword.hidden = labelPassword.hidden = targetPage.hidden = false;
+                break;
+            case constant.CrawlingOptions.GOOGLE_IMAGE.value:
+                inputKeyword.placeholder = constant.CrawlingOptions.GOOGLE_IMAGE.placeholder;
+                labelKeyword.innerHTML = requireField + constant.CrawlingOptions.GOOGLE_IMAGE.inputLabel;
+                break;
         }
+        inputKeyword.disabled = false;
     })
-    
+
+    // Handle for Facebook search (Require username)
+    inputKeyword.addEventListener("keyup", () => checkValidForm())
+    inputPassword.addEventListener("keyup", () => checkValidForm())
+    inputTargetPage.addEventListener("keyup", () => checkValidForm())
+    inputQuantity.addEventListener("keyup", () => checkValidForm())
 });
