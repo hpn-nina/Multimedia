557e0fc148a7edb1989ee44ad58049974d475c4e
Darius-21 <19521400@gm.uit.edu.vn>
2022-04-07 21:50:44 +0700

    news fix

--- CrawlHTMLLab/crawler/news.py
+++ CrawlHTMLLab/crawler/news.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 import os
 import time
@@ -15,7 +16,6 @@ def getLink(keyword, quantity):
     objBFSoup = BeautifulSoup(res.text, 'html.parser')  
     tag_h3 = objBFSoup.find_all('h3', attrs={'class': 'title-news'})
 
-    sl = 0
     for tag_a_item in tag_h3:
         link_ = tag_a_item.a.get('href')
         if link_.startswith('https://vnexpress.net/topic'):
@@ -23,14 +23,13 @@ def getLink(keyword, quantity):
         if link_.startswith('https://video'):
             continue
         link_list.append(link_)
-        sl = sl + 1
-        if sl == quantity:
+        if len(link_list) == quantity:
             break
 
 
 
-# get article title
-def getArticleTitle(link):
+def getArticle(link):
+    # get title
     res_ = requests.get(link)
     soup = BeautifulSoup(res_.text, 'html.parser')
     tag_div = soup.find_all('div', attrs={'class': 'sidebar-1'})
@@ -41,17 +40,14 @@ def getArticleTitle(link):
         for tag_div_h1 in tag_div:
             tag_h1 = tag_div_h1.find_all('h1')
             for tag_h1_title in tag_h1:
-                return tag_h1_title.get_text()
+                title = tag_h1_title.get_text()
     else:
         for tag_div_h1 in tag_div:
             tag_h1 = tag_div_h1.find_all('h1')
             for tag_h1_title in tag_h1:
-                return tag_h1_title.get_text()
-           
+                title = tag_h1_title.get_text()
 
-
-# get article content
-def getArticleContent(link):
+    # get content
     content_ = []
     res_ = requests.get(link)
     soup = BeautifulSoup(res_.text, 'html.parser')
@@ -66,26 +62,20 @@ def getArticleContent(link):
                 tag_p = tag_article_p.find_all('p')
         for tag_p_text in tag_p:
             content_.append(tag_p_text.get_text())
-        return content_
     else:
         for tag_div_article in tag_div:
             tag_article = tag_div_article.find_all('article')
             for tag_article_p in tag_article:
                 content_.append(tag_article_p.get_text())
-        return content_
-
-
-
-comment = {}
-# get Comment
-def getComment(link):
-    #current_path = os.getcwd()
-    driver_path = "D:\chromedriver_win32\chromedriver.exe"
+    
+    # get comment
+    driver_path = "./crawler/driver/chromedriver"
     driver = webdriver.Chrome(executable_path=driver_path)
     driver.get(link)
+    comment = {}
     #driver.maximize_window()
     xpath = "//*[@id='list_comment']"
-    try: 
+    try:
         cmt = driver.find_element(by=By.XPATH, value=xpath)
         driver.execute_script("arguments[0].scrollIntoView();", cmt)
         html = driver.page_source
@@ -98,28 +88,21 @@ def getComment(link):
             #comment.append(user_name)
             cnt = tag_p.contents[1].strip()
             comment[user_name] = str(cnt)
-    except:
-        return comment
-    return comment
+    except NoSuchElementException:
+        comment = 'There is no comment on this post'
 
-# get article + comment
-def getFullArticle(link):
-    article = {"title": [], "content": [], "comment": []}
-    article["title"].append(getArticleTitle(link))
-    article["content"].append(getArticleContent(link))
-    article["comment"].append(getComment(link))
-    return article
+    return {
+        "title" : title,
+        "content" : content_,
+        "comment" : comment
+    }
 
 
-keyword_ = "thích"
-link_='https://vnexpress.net/sat-thu-thich-lo-minh-tren-amazon-4444751.html'
 def crawl(keyword, quantity):
     full = []
     getLink(keyword_, quantity)
     for crawl_ in link_list:
-        full.append(getFullArticle(crawl_))
+        full.append(getArticle(crawl_))
     return full
-
-print(crawl(keyword_, 3))
 
 
